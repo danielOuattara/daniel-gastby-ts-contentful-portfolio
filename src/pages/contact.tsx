d2baaaa5043b55c215f7438c7408777a25b120e1
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-14 15:03:56 +0100

    contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,9 +3,49 @@ import type { HeadFC, PageProps } from "gatsby";
 
 export default function ContactPage(props: PageProps) {
   return (
-    <main>
-      <h1>Contact Page</h1>
-    </main>
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>stay in touch ? (Not Set Yet)</h3>
+        <form
+          className="form contact-form"
+          action="https://formspree.io/f/mdorpabo"
+          method="POST"
+        >
+          <div className="form-group">
+            <label htmlFor="name">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="name"
+                className="form-control"
+              />
+            </label>
+            <label htmlFor="email">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email"
+                className="form-control"
+              />
+            </label>
+            <label htmlFor="message">
+              <textarea
+                name="message"
+                id="message"
+                rows={7}
+                className="form-control"
+                placeholder="your message"
+              ></textarea>
+            </label>
+          </div>
+          <button type="submit" className="submit-btn btn">
+            submit
+          </button>
+        </form>
+      </article>
+    </section>
   );
 }
 
