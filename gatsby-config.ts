fc3b401317e2c0c19697173c3ca9723327391b66
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-14 14:19:11 +0100

    about page

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,8 +5,38 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `daniel-gastby-ts-contentful-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
+    // title: `daniel-gastby-ts-contentful-portfolio`,
+    title: "Fullstack Portfolio",
+    // siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: "https://daniel-gatsby-portfolio.netlify.app",
+    description:
+      "Portfolio Gatsby & Contentful powered website where I present myself and show my skills on different kind of projects and using a wide range of effective web design technologies.",
+    titleTemplate: `%s | Fullstack Portfolio `,
+    image: "/homeImage.png",
+    linkedInUsername: "daniel-b-ouattara-677146ab",
+    twitterUsername: "@danielOuattara",
+    name: "Daniel Ouattara",
+    date: "Sept. 2023",
+    aboutMe: {
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi commodiaperiam officiis nostrum maxime reprehenderit, totam eaque corporisrepudiandae soluta quos neque omnis dolore quibusdam aliquam,consectetur excepturi error suscipit.",
+      stack: [
+        "html",
+        "javascript",
+        "typescript",
+        "node",
+        "deno",
+        "express",
+        "postgresql",
+        "mongodb",
+        "css",
+        "sass",
+        "react",
+        "vue",
+        "gatsby",
+        "next",
+      ],
+      image: "./src/assets/images/hero2.svg",
+    },
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,47 @@
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import { graphql, type HeadFC, type PageProps } from "gatsby";
+import image from "./../assets/images/hero2.svg";
+import { Title, Seo } from "../components";
+// import { StaticImage } from "gatsby-plugin-image";
 
-export default function AboutPage(props: PageProps) {
+export const query = graphql`
+  query AboutMe {
+    site {
+      siteMetadata {
+        aboutMe {
+          text
+          stack
+          image
+        }
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }: PageProps<TypeAboutMeQuery>) {
+  const about = data.site.siteMetadata.aboutMe;
+
   return (
-    <main>
-      <h1>About Page</h1>
-    </main>
+    <section className="about-page">
+      <div className="section-center about-center">
+        <img src={image as unknown as string} alt="about" />
+        {/* <StaticImage
+          src="./../assets/images/hero2.svg"
+          alt="portfolio"
+          // className="hero-img"
+          placeholder="blurred"
+        /> */}
+        <article className="about-text">
+          <Title title={"about"} />
+          <p>{about.text}</p>
+          <div className="about-stack">
+            {about.stack.map((item) => (
+              <span key={item}>{item}</span>
+            ))}
+          </div>
+        </article>
+      </div>
+    </section>
   );
 }
 
