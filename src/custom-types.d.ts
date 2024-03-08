671d7caa03523d943f4cfc9d71aa4e9ff0f1702f
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-08 18:23:10 +0100

    add custom types: sidebar + navbar logic

--- src/custom-types.d.ts
+++ src/custom-types.d.ts
@@ -1,3 +1,5 @@
+import { page_links } from "./constants";
+
 type TypeAboutMeQuery = {
   site: {
     siteMetadata: {
@@ -8,4 +10,56 @@ type TypeAboutMeQuery = {
       };
     };
   };
+};
+
+type TypeSubMenuLink = {
+  id: string;
+  url: string;
+  page: string;
+};
+
+type TypeSubMenuPageToShow = {
+  id: number;
+  url: string;
+  page: string;
+  subLinks: TypeSubMenuLink[];
+};
+
+type TypeNavbar = {
+  toggleSidebar: () => void;
+  setIsSubMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setSubMenuLocation: React.Dispatch<
+    React.SetStateAction<{
+      subMenuCenterPosition: number;
+      subMenuTopPosition: number;
+    }>
+  >;
+  setSubMenuPageToShow: React.Dispatch<
+    React.SetStateAction<{
+      id: number;
+      url: string;
+      page: string;
+      subLinks: TypeSubMenuLink[];
+    }>
+  >;
+};
+
+type TypeSideBar = {
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+};
+
+type TypeSubmenu = {
+  isSubMenuOpen: boolean;
+  subMenuLocation: {
+    subMenuCenterPosition: number;
+    subMenuTopPosition: number;
+  };
+  subMenuPageToShow: {
+    id: number;
+    url: string;
+    page: string;
+    subLinks: TypeSubMenuLink[];
+  };
+  setIsSubMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
