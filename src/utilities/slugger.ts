8346168696df1deb43e3991c70666f7699119e48
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-07 22:17:50 +0200

    update slugger utility

--- src/utilities/slugger.ts
+++ src/utilities/slugger.ts
@@ -2,6 +2,8 @@ import slugify from "slugify";
 
 export function slugger(text: string) {
   return slugify(text, {
+    replacement: "-",
+    remove: /[*+~\,.()'"!:@]/g,
     lower: true,
     trim: true,
   });
